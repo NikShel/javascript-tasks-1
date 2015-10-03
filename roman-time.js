1fdf9305a34a3d9fb913d61043043c574d94db51
NikShel <nikshelekb@yandex.ru>
2015-10-03 13:44:04 +0500

    Made task

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,33 @@
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-// Немного замечательного кода и магии
+var romanNumbers = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
+var numbers = [1, 4, 5, 9, 10, 40, 50];
+
+function toRoman(number) {
+    if (number == 0) {
+        return '-';
+    }
+    var result = '';
+    var i = numbers.length - 1;
+    while (number > 0) {
+        if (number < numbers[i]) {
+            i--;
+        } else {
+            number -= numbers[i];
+            result += romanNumbers[i];
+        }
+    }
+    return result;
+}
+
+if (!(hours >= 0 && hours < 24 && minutes >=0 && minutes < 60)) {
+    console.log('Время указано не верно')
+}
+else {
+    romanHours = toRoman(hours);
+    romanMinutes = toRoman(minutes);
+    console.log(romanHours + ':' + romanMinutes);
+}
+
+
